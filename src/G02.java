20ba847bf8fd78971b52ddd20482fd9507a64eac
DeryaDursun <derdursun55@gmail.com>
2023-03-29 22:33:44 +0300

    4.gün

---
+++ src/G02.java
@@ -0,0 +1,11 @@
+public class G02 {
+    public static void main(String[] args) {
+        // git init
+        //git add .
+        //git commit -m "versiyon1"
+        //git remote add arijin repoURL adresi
+        //git remote set -url repoURL adresi
+
+        //git push
+    }
+}
